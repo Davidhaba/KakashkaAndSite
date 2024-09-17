165c58d25cc193f11d5a3f4be401da1c02d644cf
Davidhaba <154765655+Davidhaba@users.noreply.github.com>
2024-09-17 16:56:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -392,7 +392,7 @@ def main():
     loop.run_until_complete(app.run_polling())
 
 if __name__ == '__main__':
-    thread = Thread(target=lambda: appFlask.run(debug=True, use_reloader=False))
+    thread = Thread(target=lambda: appFlask.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False))
     thread.start()
     main()
 
